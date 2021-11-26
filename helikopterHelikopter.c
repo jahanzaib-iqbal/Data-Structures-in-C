a8704ba6e0d806fbf7914b8dbaf9de2e2e54a1bf
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2021-11-26 23:11:43 +0500

    Add files via upload

---
+++ helikopterHelikopter.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    //this program is for fun and its new trnding meme song Helikopter Helikopter...the program simply run a helicopter
+    int ch = 1; 
+    while (ch > 0)
+    {
+        printf("\nROFL:ROFL:LOL:\n");
+        printf(" L     /-----------\n");
+        printf("LOL=====         []\\\n");
+        printf("        \\           \\\n");
+        printf("         \\___________]\n");
+        printf("             I      I\n");
+        printf("        ----------------/\n");
+
+        system("cls");
+
+        printf("\n          LOL:\n");
+        printf("       /-----------\n");
+        printf("LOL=====         []\\\n");
+        printf("        \\           \\\n");
+        printf("         \\___________]\n");
+        printf("             I      I\n");
+        printf("        ----------------/\n");
+
+        system("cls");
+
+        printf("\n         :LOL:ROL:ROFL\n");
+        printf("       /-----------\n");
+        printf("LOL=====         []\\\n");
+        printf(" L      \\           \\\n");
+        printf("         \\___________]\n");
+        printf("             I      I\n");
+        printf("        ----------------/\n");
+
+        system("cls");
+    }
+    return 0;
+}
